f7153ab17a98e42fc0d50680c94804f54815c476
ldgonzalez26 <ldgonzalezmedina@gmail.com>
2024-03-30 21:07:24 -0500

    adding azure functions definition
    Signed-off-by: ldgonzalez26 <ldgonzalezmedina@gmail.com>

--- http-get-product-by-id/index.ts
+++ http-get-product-by-id/index.ts
@@ -1,17 +1,100 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
+const ProductsList = [
+    {
+      description: 'test1',
+      id: '1234',
+      price: 24,
+      title: 'ProductOne',
+      count: 1,
+    },
+    {
+      description: 'test2',
+      id: '12345',
+      price: 24,
+      title: 'ProductOne',
+      count: 1,
+    },
+    {
+      description: 'test3',
+      id: '123456',
+      price: 24,
+      title: 'ProductOne',
+      count: 1,
+    },
+    {
+      description: 'test4',
+      id: '123457',
+      price: 24,
+      title: 'ProductOne',
+      count: 1,
+    },
+    {
+      description: 'Short Product Description7',
+      id: '7567ec4b-b10c-48c5-9345-fc73c48a80a1',
+      price: 15,
+      title: 'ProductTitle',
+      count: 1,
+    },
+    {
+      description: 'Short Product Description2',
+      id: '7567ec4b-b10c-48c5-9345-fc73c48a80a3',
+      price: 23,
+      title: 'Product',
+      count: 1,
+    },
+    {
+      description: 'Short Product Description4',
+      id: '7567ec4b-b10c-48c5-9345-fc73348a80a1',
+      price: 15,
+      title: 'ProductTest',
+      count: 1,
+    },
+    {
+      description: 'Short Product Descriptio1',
+      id: '7567ec4b-b10c-48c5-9445-fc73c48a80a2',
+      price: 23,
+      title: 'Product2',
+      count: 1,
+    },
+    {
+      description: 'Short Product Description7',
+      id: '7567ec4b-b10c-45c5-9345-fc73c48a80a1',
+      price: 15,
+      title: 'ProductName',
+      count: 1,
+    },
+  ];
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
+    if(!context.bindingData.productId){
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            status:400,
+            body: {
+                message:"Missing id"
+            }
+        };
+        return;
+    }
+    let id = context.bindingData.productId;
+    if(typeof(context.bindingData.productId) === 'number'){
+        id = id.toString();
+    }
+    let item = ProductsList.find((item) => item.id === id)
+    if(item){
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: item
+        };
+        return;
+    }else{
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: 'item not found'
+        };
+        return;
+    }
+   
 };
 
 export default httpTrigger;
--- http-get-product-list/index.ts
+++ http-get-product-list/index.ts
@@ -1,15 +1,77 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
+const ProductsList = [
+    {
+      description: 'test1',
+      id: '1234',
+      price: 24,
+      title: 'ProductOne',
+      count: 1,
+    },
+    {
+      description: 'test2',
+      id: '12345',
+      price: 24,
+      title: 'ProductOne',
+      count: 1,
+    },
+    {
+      description: 'test3',
+      id: '123456',
+      price: 24,
+      title: 'ProductOne',
+      count: 1,
+    },
+    {
+      description: 'test4',
+      id: '123457',
+      price: 24,
+      title: 'ProductOne',
+      count: 1,
+    },
+    {
+      description: 'Short Product Description7',
+      id: '7567ec4b-b10c-48c5-9345-fc73c48a80a1',
+      price: 15,
+      title: 'ProductTitle',
+      count: 1,
+    },
+    {
+      description: 'Short Product Description2',
+      id: '7567ec4b-b10c-48c5-9345-fc73c48a80a3',
+      price: 23,
+      title: 'Product',
+      count: 1,
+    },
+    {
+      description: 'Short Product Description4',
+      id: '7567ec4b-b10c-48c5-9345-fc73348a80a1',
+      price: 15,
+      title: 'ProductTest',
+      count: 1,
+    },
+    {
+      description: 'Short Product Descriptio1',
+      id: '7567ec4b-b10c-48c5-9445-fc73c48a80a2',
+      price: 23,
+      title: 'Product2',
+      count: 1,
+    },
+    {
+      description: 'Short Product Description7',
+      id: '7567ec4b-b10c-45c5-9345-fc73c48a80a1',
+      price: 15,
+      title: 'ProductName',
+      count: 1,
+    },
+  ];
+  
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: {
+            data:ProductsList
+        }
     };
 
 };
